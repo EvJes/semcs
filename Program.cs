c357111dbfc890bbebb31453122f09449344d081
Alexandr <asters777@yandex.ru>
2023-01-06 12:27:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//  1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите число a");
+int numa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int numb = Convert.ToInt32(Console.ReadLine());
+if(numa % numb ==0);
+{
+    Console.WriteLine("да");
+}
+    else
+    {
+        
+    }
+
