cb1d98f3d42297c09838b4a44a29f0b788becd29
EvJes <121495739+EvJes@users.noreply.github.com>
2023-01-14 23:30:07 +0300

    Add files via upload

---
+++ dz6.cs
@@ -0,0 +1,56 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[,] GetArray(int m, int n, int min, int max)         //не работает
+{
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int row = 0; row < inArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < inArray.GetLength(1); column++)
+        {
+            Console.Write($"{inArray[row,column]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
